db9e771c340d5e75c969e2c9adecaaadcea3aedc
paarthmadan <madanhockey@hotmail.com>
2016-08-16 23:39:56 -0400

    Created Node class to represent nodes in binary tree

---
+++ src/Node.java
@@ -0,0 +1,28 @@
+
+public class Node implements Comparable<Node>{
+
+	int frequency;
+	Node left;
+	Node right;
+	char symbol;
+	
+	public Node(char symbol, int frequency, Node left, Node right){
+		this.symbol = symbol;
+		this.frequency = frequency;
+		this.left = left;
+		this.right = right;
+	}
+	
+	public boolean isLeaf(){
+		if(left == null && right == null){
+			return true;
+		}
+		return false;
+	}
+	
+	public int compareTo(Node other){
+		return this.frequency - other.frequency;
+	}
+	
+
+}
