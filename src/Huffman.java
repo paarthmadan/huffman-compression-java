729f43c51e72cd8742631ba0a49ad2c4573b6fd5
paarthmadan <madanhockey@hotmail.com>
2016-08-16 23:47:16 -0400

    created frequency array parallel to ASCII alphabet
    iterate array index of casted char in input string, to count frequency of character

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
 public class Huffman {
-	//initial commit
+	
+	//represents the total number of characters in the ASCII alphabet
+	private final static int R = 256;
+	
+	public static void main(String [] args){
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the string or file, that needs to be compressed:");
+		
+		String line = input.nextLine();
+		char [] characters = line.toCharArray();
+		
+		
+		int [] frequency = new int[R];
+		
+		for(int i = 0 ; i < characters.length; i++){
+			frequency[characters[i]]++;
+		}
+			
+	}
+	
 }
